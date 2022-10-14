b24aa81198df9b0aa0a1837cb7fab45b8c3ef5ad
Remmarg0rPA <remmarg0rpa.remmarg0rpa@gmail.com>
2022-10-14 14:57:18 +0200

    Changed to 2D vectors, use Bool instead of the Used type & added some comments

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,10 @@
 module Main where
 
 import Prelude hiding (Left, Right)
+import Data.List (sortOn)
+import Data.List.Index (imap)
 import Data.Maybe (fromJust)
-import qualified Data.Vector as V
-import Control.Lens.Lens ((??))
-
-data Used
-  = Empty
-  | Occupied
-  deriving (Show, Eq)
+import Linear (V2 (..))
 
 data Direction
  = Up
@@ -17,98 +13,95 @@ data Direction
  | Right
  deriving (Show, Eq, Enum)
 
-type Position a
-  = V.Vector a
-
-type Move a
-  = (Position a, Direction)
-
-type Board a
-  = [(Position a, Used)]
-
-instance Enum Used where
-  toEnum 0 = Empty
-  toEnum 1 = Occupied
-  fromEnum Empty    = 0
-  fromEnum Occupied = 1
-  succ Empty    = Occupied
-  succ Occupied = Empty
-  pred = succ
+type Position a = V2 a
+type Move a = (Position a, Direction)
+type Board a = [(Position a, Bool)]
 
 directions :: [Direction]
 directions = [Up, Down, Left, Right]
 
 dirToPos :: (Integral a) => Direction -> Position a
-dirToPos Up    = tupPos (0, -1)
-dirToPos Down  = tupPos (0, 1)
-dirToPos Left  = tupPos (-1, 0)
-dirToPos Right = tupPos (1, 0)
+dirToPos Up    = V2 0 (-1)
+dirToPos Down  = V2 0 1
+dirToPos Left  = V2 (-1) 0
+dirToPos Right = V2 1 0
 
 flipPos :: (Integral a) => Position a -> Board a -> Board a
-flipPos pos board = [if pos /= p then (p, s) else (p, succ s) | (p,s)<-board]
+flipPos pos board = [if pos /= p then (p, s) else (p, not s) | (p,s)<-board]
 
-tupPos :: (Integral a) => (a, a) -> Position a
-tupPos (x, y) = V.fromList [x, y]
+-- When only one point is occupied the board is considered solved
+isSolved :: (Integral a) => Board a -> Bool
+isSolved = (1==) . length . filter snd
+
+-- Checks that all 3 points involved in a move is on the board
+isValidMove :: (Integral a) => Move a -> Board a -> Bool
+isValidMove (pos, dir) board =
+  all (`elem` board) [(pos, True), (pos + d, True), (pos + 2*d, False)]
+  where
+    d = dirToPos dir
 
 makeMove :: (Integral a) => Move a -> Board a -> Maybe (Board a)
 makeMove (pos, dir) board =
-  if not (validMove (pos, dir) board)
+  if not (isValidMove (pos, dir) board)
     then Nothing
     else Just $ updateBoard board
   where
-    updateBoard = foldl1 (.) (map flipPos [pos,
-                                        V.zipWith (+) pos (dirToPos dir),
-                                        V.zipWith (+) pos (fmap (2*) (dirToPos dir))])
+    d = dirToPos dir
+    updateBoard = foldl1 (.) (map flipPos [pos, pos + d, pos + 2*d])
 
-isSolved :: (Integral a) => Board a -> Bool
-isSolved = (1==) . length . filter ((==Occupied) . snd)
-
-validMove :: (Integral a) => Move a -> Board a -> Bool
-validMove (pos, dir) board = all (`elem` board) [(fromPos, Occupied),
-                                                 (overPos, Occupied),
-                                                 (toPos, Empty)]
-  where
-    fromPos = pos
-    overPos = V.zipWith (+) pos (dirToPos dir)
-    toPos   = V.zipWith (+) pos ((2*) <$> dirToPos dir)
+-- Sorts the board based on the x positions
+sortBoard :: (Integral a) => Board a -> Board a
+sortBoard = sortOn ((\(V2 x _) -> x) . fst)
 
 printBoard :: (Integral a) => Board a -> IO()
 printBoard board =
   putStrLn $ unlines [[convChar (lookup pos board)
-                      | x<-[0..(fromIntegral width)], let pos = tupPos (x,y)]
+                      | x<-[0..width], let pos = V2 x y]
                      | y<-[0..height]]
   where
-    convChar (Just Occupied) = 'X'
-    convChar (Just Empty)    = '.'
-    convChar Nothing       = ' '
-    width  = foldl max (-1) $ map ((V.!0) . fst) board
-    height = foldl max (-1) $ map ((V.!1) . fst) board
+    convChar (Just True)  = 'X'
+    convChar (Just False) = '.'
+    convChar Nothing      = ' '
+    width  = foldl max (-1) $ map ((\(V2 x _) -> x) . fst) board
+    height = foldl max (-1) $ map ((\(V2 _ y )-> y) . fst) board
 
+-- Converts a list of strings (rows of the board) into a board
+readBoard :: (Integral a) => [String] -> Board a
+readBoard = concat . imap genPos
+  where
+    genPos i rw = [(V2 (fromIntegral x) (fromIntegral i), rw!!x=='X')
+                  | x<-[0..length rw - 1], rw!!x/=' ']
+
+-- Iterates through all directions for all occupied points on the board
 bruteForceSolution :: (Integral a) => Board a -> Maybe [Move a]
 bruteForceSolution board
   | isSolved board = Just []
   | null allValidMoves = Nothing
   | otherwise =
-      let brds = filter ((/=Nothing) . fst) [(bruteForceSolution (fromJust $ makeMove mv board), mv) | mv<-allValidMoves] in
-        case brds of
-          [] -> Nothing
-          (Just mvs, mv):_ -> Just (mv:mvs)
+      case solution of
+        (sol, mv):_ -> (mv:) <$> sol -- prepend last move to solution
+        _ -> Nothing
   where
-    occupiedSquares = map fst $ filter ((==Occupied) . snd) board
-    allValidMoves = filter (`validMove` board) [(pos, dir) | pos<-occupiedSquares, dir<-directions]
+    usedSqrs = map fst . filter snd $ board -- Coordinates for occupied squares
+    allValidMoves = filter (`isValidMove` board) [(pos, dir)
+                                                 | pos<-usedSqrs, dir<-directions]
+    solution = filter (not . null . fst) [(bruteForceSolution (fromJust $ makeMove mv board), mv)
+                                         | mv<-allValidMoves]
 
--- The standard board
-board :: (Integral a) => Board a
-board = [(tupPos (x,y), if x==3 && y==3 then Empty else Occupied) | x<-[0..6], y<-[0..6], inBounds (tupPos (x,y))]
-
--- Specific to the standard board
-inBounds :: (Integral a) => Position a -> Bool
-inBounds = or . (??) [p [2..4] [0..1],
-                      p [0..6] [2..4],
-                      p [2..4] [5..6]]
+-- The board is sorted to make finding a solution quicker
+standardBoard :: (Integral a) => Board a
+standardBoard = sortBoard $ readBoard brd
   where
-    p xR yR v = (v V.! 0) `elem` xR && (v V.! 1) `elem` yR
+    brd = ["  XXX  ",
+           "  XXX  ",
+           "XXXXXXX",
+           "XXX.XXX",
+           "XXXXXXX",
+           "  XXX  ",
+           "  XXX  "]
 
 main :: IO()
 main = do
+  let board = standardBoard
+  printBoard board
   print $ bruteForceSolution board
