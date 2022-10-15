dd1eed97031fd56e8f5bf93e6f8ebb9526c6484f
Remmarg0rPA <remmarg0rpa.remmarg0rpa@gmail.com>
2022-10-15 17:11:17 +0200

    A board is now a Map

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
-import Data.List (sortOn)
 import Data.List.Index (imap)
+import qualified Data.Map as M
 import Data.Maybe (fromJust)
 import Linear (V2(..))
 import Prelude hiding (Left, Right)
@@ -17,10 +17,10 @@ type Position a = V2 a
 
 type Move a = (Position a, Direction)
 
-type Board a = [(Position a, Bool)]
+type Board a = M.Map (Position a) Bool
 
 directions :: [Direction]
-directions = [Up, Down, Left, Right]
+directions = [Up .. Right]
 
 dirToPos :: (Integral a) => Direction -> Position a
 dirToPos Up = V2 0 (-1)
@@ -29,21 +29,18 @@ dirToPos Left = V2 (-1) 0
 dirToPos Right = V2 1 0
 
 flipPos :: (Integral a) => Position a -> Board a -> Board a
-flipPos pos board =
-    [ if pos /= p
-        then (p, s)
-        else (p, not s)
-    | (p, s) <- board
-    ]
+flipPos = M.adjust not
 
 -- When only one point is occupied the board is considered solved
 isSolved :: (Integral a) => Board a -> Bool
-isSolved = (1 ==) . length . filter snd
+isSolved = (1==) . M.foldr (\x acc -> if x then acc+1 else acc) 0
 
--- Checks that all 3 points involved in a move is on the board
+-- Checks that all 3 positions involved in a move is on the board
 isValidMove :: (Integral a) => Move a -> Board a -> Bool
 isValidMove (pos, dir) board =
-    all (`elem` board) [(pos, True), (pos + d, True), (pos + 2 * d, False)]
+    M.findWithDefault False pos board &&
+    M.findWithDefault False (pos + d) board &&
+    not (M.findWithDefault True (pos + 2 * d) board)
   where
     d = dirToPos dir
 
@@ -56,35 +53,8 @@ makeMove (pos, dir) board =
     d = dirToPos dir
     updateBoard = foldl1 (.) (map flipPos [pos, pos + d, pos + 2 * d])
 
--- Sorts the board based on the x positions
-sortBoard :: (Integral a) => Board a -> Board a
-sortBoard = sortOn ((\(V2 x _) -> x) . fst)
-
-printBoard :: (Integral a) => Board a -> IO ()
-printBoard board =
-    putStrLn $
-    unlines
-        [ [convChar (lookup pos board) | x <- [0 .. width], let pos = V2 x y]
-        | y <- [0 .. height]
-        ]
-  where
-    convChar (Just True) = 'X'
-    convChar (Just False) = '.'
-    convChar Nothing = ' '
-    width = foldl max (-1) $ map ((\(V2 x _) -> x) . fst) board
-    height = foldl max (-1) $ map ((\(V2 _ y) -> y) . fst) board
-
--- Converts a list of strings (rows of the board) into a board
-readBoard :: (Integral a) => [String] -> Board a
-readBoard = concat . imap genPos
-  where
-    genPos i rw =
-        [ (V2 (fromIntegral x) (fromIntegral i), rw !! x == 'X')
-        | x <- [0 .. length rw - 1]
-        , rw !! x /= ' '
-        ]
-
 -- Iterates through all directions for all occupied points on the board
+-- and returns the first solution found
 bruteForceSolution :: (Integral a) => Board a -> Maybe [Move a]
 bruteForceSolution board
     | isSolved board = Just []
@@ -94,11 +64,14 @@ bruteForceSolution board
             (sol, mv):_ -> (mv :) <$> sol -- prepend last move to solution
             _ -> Nothing
   where
-    usedSqrs = map fst . filter snd $ board -- Coordinates for occupied squares
+    -- Coordinates of occupied positions
+    usedSqrs = map fst . M.toList . M.filter id $ board
+    -- Filters out the valid moves from all of the possible combinations
     allValidMoves =
         filter
             (`isValidMove` board)
             [(pos, dir) | pos <- usedSqrs, dir <- directions]
+    -- Picks the first solution found
     solution =
         filter
             (not . null . fst)
@@ -106,9 +79,32 @@ bruteForceSolution board
             | mv <- allValidMoves
             ]
 
--- The board is sorted to make finding a solution quicker
+printBoard :: (Integral a) => Board a -> IO ()
+printBoard board =
+    putStrLn $
+    unlines
+        [ [convChar (M.lookup pos board) | x <- [0 .. width], let pos = V2 x y]
+        | y <- [0 .. height]
+        ]
+  where
+    convChar (Just True) = 'X'
+    convChar (Just False) = '.'
+    convChar Nothing = ' '
+    width = foldl max (-1) . map (\(V2 x _) -> x) . M.keys $ board
+    height = foldl max (-1) . map (\(V2 _ y) -> y) . M.keys $ board
+
+-- Converts a list of strings (rows of the board) into a board
+readBoard :: (Integral a) => [String] -> Board a
+readBoard = M.fromList . concat . imap genPos
+  where
+    genPos i rw =
+        [ (V2 (fromIntegral x) (fromIntegral i), rw !! x == 'X')
+        | x <- [0 .. length rw - 1]
+        , rw !! x /= ' '
+        ]
+
 standardBoard :: (Integral a) => Board a
-standardBoard = sortBoard $ readBoard brd
+standardBoard = readBoard brd
   where
     brd =
         [ "  XXX  "
